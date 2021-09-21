eb40e4a5f9848c363bc3d64cec9a6bcb61cb8997
LLumisS <vasa16671@gmail.com>
2021-09-21 16:55:07 +0300

    All exercises

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+const generateKey = (length, characters) => {
+  let res = '';
+  const letters = characters.split('');
+  for (let i = 0; i < length; i++) {
+    res += letters[Math.floor(Math.random() * characters.length)];
+  }
+  return res;
 };
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
 
 module.exports = { generateKey };
--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,11 @@
 'use strict';
 
-const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: x => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+const methods = (object) => {
+  const array = new Array();
+  for (const name in object)
+    if (typeof object[name] === 'function')
+      array.push([name, object[name].length]);
+  return array;
 };
 
 module.exports = { methods };
